dc5bba55d523eabda10240eabfdcbd3b96b80444
Zeeshan Khan <zeeshankhan@Zeeshans-MacBook-Pro.local>
2015-02-28 11:46:54 +0530

    Added resource links

--- ZKSSLPinning/ZKSSLPinning/ZKSSLHandler.h
+++ ZKSSLPinning/ZKSSLPinning/ZKSSLHandler.h
@@ -6,6 +6,14 @@
 //  Copyright (c) 2015 Zeeshan. All rights reserved.
 //
 
+// Content across the web about OpenSSL
+// https://zakird.com/2013/10/13/certificate-parsing-with-openssl/
+// http://stackoverflow.com/questions/14645779/parsing-certificate-using-openssl-x509-h
+// https://www.digicert.com/ssl.htm
+// https://www.duosecurity.com/blog/working-around-phoney-ssl-certificates-on-ios-with-openssl
+// https://github.com/akgood/iOSBasicConstraintsWorkaround
+
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
